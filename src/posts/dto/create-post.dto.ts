9a95906ac848380591fc91ab8e32c1f40056bf9f
Nurken <lider.2005.1@gmail.com>
2026-03-02 16:54:15 +0500

    adding validation to dto

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,5 +1,14 @@
+import { IsString,IsNotEmpty,Length, IsInt } from "class-validator";
+
+
 export class CreatePostDto{
+    @IsString()
+    @IsNotEmpty()
+    @Length(5,100)
     readonly title:string;
+    @IsString()
+    @IsNotEmpty()
     readonly content:string;
+    @IsInt()
     readonly authorId:number;
 }
